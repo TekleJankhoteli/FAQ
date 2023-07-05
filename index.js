239d5c1ea9a2414a1708ec99d737146aee64850b
TekleJankhoteli <jankhotelitekla@gmail.com>
2023-07-06 02:55:54 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let box=document.querySelectorAll(".box");
+let boxBottom=document.querySelectorAll(".boxBottom");
+let arrow=document.querySelectorAll(".arrow");
+let answer=document.querySelectorAll(".answer");
+let question=document.querySelectorAll(".question")
+
+
+for (let i=0; i<box.length; i++){
+    arrow[i].addEventListener("click", ()=>{
+        boxBottom[i].classList.toggle('act');
+        arrow[i].classList.toggle('rot');
+        question[i].classList.toggle('bold')
+        box[i].classList.toggle('NB')
+
+       
+
+    })
+}
+
+
+
+
